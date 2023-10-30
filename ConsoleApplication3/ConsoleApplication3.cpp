022ace060b4483fc6d478bee4a99c86771daee35
Kamend119 <a.mikryukova119@mail.ru>
2023-10-30 07:41:34 +0500

    попытка создания класса

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <math.h>
+#include <vector>
 using namespace std;
 
+/*
 void task1()
 {
 	// точность вычисления
@@ -182,6 +184,220 @@ void task2()
 		cout << "Не сходится";
 
 }
+*/
+
+class method_simple_iterations
+{
+private:
+
+	float** systems = new float* [count_equation];
+
+	// Количество уровнений
+	int count_equation;
+
+	// Колисчество элементов в уровнениях
+	int count_element;
+
+	// Точность вычисления
+	float eps = 0;
+
+	//Начало работы
+	void getting_started()
+	{
+		do {
+			cout << "Задайте количество уровнений: ";
+			cin >> count_equation;
+		} while (count_equation <= 0);
+
+		do {
+			cout << "Задайте количество переменных в уровнении: ";
+			cin >> count_element;
+		} while (count_element <= 0);
+
+		do {
+			cout << "Задайте точность вычисления: ";
+			cin >> eps;
+		} while (eps <= -1);
+
+	}
+
+	/*vector<vector<float>> array(count_equation, vector<float>(count_element));
+	vector<vector<float>> systems(count_equation);
+	systems.push_back(vector<float>());*/
+
+	// Заполнение уравнений системы
+	void filling_system()
+	{
+		for (int i = 0; i < count_equation; i++)
+		{
+			for (int j = 0; j < count_element; j++)
+			{
+				cout << "Задайте " << (j + 1) << " переменную " << (i + 1) << " урaвнения: ";
+				cin >> systems[i][j];
+			}
+		}
+	}
+	
+	// Свободные члены
+	float* z = new float [count_equation];
+
+	// Построчные нормы матрицы
+	float* norma_martitz = new float[count_equation];
+
+	// Норма матрицы
+	float c = 0;
+
+	// Нулевое приближение n
+	float* x0 = new float[count_equation];
+
+	// Нулевое приближение n+1
+	float* x1 = new float[count_equation];
+
+	// Условие остановки для каждого примера
+	float* ostan = new float[count_equation];
+
+	// Нормы матриц
+	void norma_martitzs()
+	{
+		// Построчные нормы матрицы
+		for (int i = 0; i < count_equation; i++)
+		{
+			for (int n = 0; n < count_equation; n++)
+				for (int m = 0; m < count_element; m++)
+				{
+					if (i != m)
+						norma_martitz[i] += systems[n][m];
+				}
+			
+			norma_martitz[i] = norma_martitz[i] / systems[i][i];
+		}
+
+		float max = -100000000000000.00;
+
+		for (int i = 0; i < count_equation; i++)
+		{
+			if (abs(norma_martitz[i]) > max)
+			{
+				max = norma_martitz[i];
+			}
+		}
+
+		// Норма матрицы
+		c = max;
+	}
+
+	// Условие остановки
+	void stop_condition()
+	{
+		for (int i = 0; i < count_equation; i++)
+		{
+			ostan[i] = c / (1 - c) * fabs(x0[i] - x1[i]);
+		}
+	}
+
+	// нулевое приближение n
+	void zero_approximation()
+	{
+		// x(n)
+		for (int i = 0; i < count_equation; i++)
+		{
+			x0[i] = z[i] / systems[i][i];
+		}
+	}
+
+	void clear()
+	{
+		// удаление массивов  
+		
+		delete [] z;
+		delete [] x0;
+		delete [] x1;
+		delete [] norma_martitz;
+		delete [] ostan;
+	}
+	
+
+public:
+	
+	/*method_simple_iterations(int p_count_equation, int p_count_element)
+	{
+		count_equation = p_count_equation;
+		count_element = p_count_element;
+
+		float** systems = new float*[count_equation];
+		for (int i = 0; i < count_element; i++)
+			systems[i] = new float[count_element];
+	}*/
+
+	void calculation()
+	{
+
+		//Начало работы
+		getting_started();
+		// Заполнение уравнений системы
+		filling_system();
+		// Нормы матриц
+		norma_martitzs();
+
+		if (c < 1 && c > 0)
+		{
+			// нулевое приближение n
+			zero_approximation();
+
+			bool ostanovka;
+
+			do {		
+				for (int i = 0; i < count_equation; i++)
+				{
+					for (int n = 0; n < count_equation; n++)
+						for (int m = 0; m < count_element; m++)
+						{
+							if (i != m)
+								x1[i] += (systems[n][m] * x0[n]);
+						}
+
+					x1[i] = z[i] - x1[i];
+				}
+
+				// Условие остановки
+				stop_condition();
+
+				for (int i = 0; i < count_equation; i++)
+				{
+					x0[i] = x1[i];
+
+					if (ostan[i] < eps)
+						ostanovka = true;
+				}
+
+			} while (ostanovka == false);
+
+			cout << "Ответ: ";
+
+			for (int i = 0; i < count_equation; i++)
+			{
+				cout << "x(n):" << x0[i] << "Оценка:" << ostan[i];
+			}
+		}
+
+		else
+			cout << "Система не сходится";
+
+		clear();
+	}
+	
+	/*~method_simple_iterations()
+	{
+		// удаление массивов  
+		for (int i = 0; i < count_equation; i++)
+		{
+			delete[] systems[i];
+		}
+
+		delete[] systems;
+	}*/
+};
+
 
 int main()
 {
@@ -191,7 +407,13 @@ int main()
 	//task1();
 
 	//метод зейделя
-	task2();
+	//task2();
 
+	method_simple_iterations task1/*(4, 4)*/;
+	task1.calculation();
+
+	cin.get();
+	cin.get();
+	cin.get();
 	return 0;
 }
